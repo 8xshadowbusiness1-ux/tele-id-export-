0aaa206ff1a260695268eb10bc6ee102248cb0c5
8xshadowbusiness1-ux <8xshadowbusiness1@gmail.com>
2025-11-05 15:37:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# ==============================
-# ✅ Final Render start script
-# ==============================
-
 echo "🚀 Installing dependencies..."
-pip install --no-cache-dir --no-binary :all: telethon==1.37.0 requests==2.32.3
+pip install --no-cache-dir telethon==1.37.0 requests==2.32.3
 
 echo "✅ Dependencies installed successfully."
 echo "💓 Starting Telegram Member Extractor..."
-
 python3 bot_controlled_fetcher.py
